51bcdee45463a1428badf614ec893d7fdd186054
Akhil331 <ak53@iitbbs.ac.in>
2025-01-24 14:57:17 +0530

    added profile section to sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,89 +1,34 @@
-"use client";
-
 import * as React from "react";
-import { usePathname } from "next/navigation";
+import { Sidebar, SidebarFooter, SidebarRail } from "@/components/ui/sidebar";
+import { NavUser } from "./nav-user";
+import SidebarMenuContent from "./sidebar-menu";
+import { auth } from "@/auth";
 
-import { VersionSwitcher } from "@/components/version-switcher";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarRail,
-} from "@/components/ui/sidebar";
+interface CustomSession {
+  name: string;
+  email: string;
+  avatar: string;
+}
 
-// This is sample data.
-const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
-  navMain: [
-    {
-      title: "Menu Items",
-      url: "#",
-      items: [
-        {
-          title: "Home",
-          url: "/home",
-        },
-        {
-          title: "Collections",
-          url: "/collections",
-        },
-        {
-          title: "Find Jobs",
-          url: "/find-jobs",
-        },
-        {
-          title: "Tags",
-          url: "/tags",
-        },
-        {
-          title: "Communities",
-          url: "/communities",
-        },
-        {
-          title: "Ask a Question",
-          url: "/ask-a-question",
-        },
-      ],
-    },
-  ],
-};
+export async function AppSidebar({
+  ...props
+}: React.ComponentProps<typeof Sidebar>) {
+  const authSession = await auth();
+  const session: CustomSession = {
+    name: authSession?.user?.name ?? "Sign In",
+    email: authSession?.user?.email ?? "user@example.com",
+    avatar: authSession?.user?.image ?? "/avatar.png",
+  };
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  const pathname = usePathname();
   return (
     <Sidebar {...props}>
-      <SidebarHeader className="mt-4 px-4">
-        <VersionSwitcher
-          versions={data.versions}
-          defaultVersion={data.versions[0]}
-        />
-      </SidebarHeader>
-      <SidebarContent className="mt-4 px-2">
-        {/* We create a SidebarGroup for each parent. */}
-        {data.navMain.map((item) => (
-          <SidebarGroup key={item.title}>
-            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {item.items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={pathname === item.url}>
-                      <a href={item.url}>{item.title}</a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        ))}
-      </SidebarContent>
+      <SidebarMenuContent />
       <SidebarRail />
+      <div className="mt-auto">
+        <SidebarFooter>
+          <NavUser user={session} />
+        </SidebarFooter>
+      </div>
     </Sidebar>
   );
 }
